73a52f41570d365e81ba08e24e143f99c31767ab
airlovelq <34851208+airlovelq@users.noreply.github.com>
2019-06-14 16:52:08 +0800

    Add files via upload

---
+++ AnnealingAlgorithm.cpp
@@ -0,0 +1,50 @@
+// AnnealingAlgorithm.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "AnnealingAlgorithm.h"
+#include <time.h>
+
+
+
+CAnnealingAlgorithm::CAnnealingAlgorithm()
+{
+}
+
+
+CAnnealingAlgorithm::~CAnnealingAlgorithm()
+{
+}
+
+void CAnnealingAlgorithm::Generate()
+{
+	srand((unsigned int)time(0));
+	m_best = func(rnd(0.0, 2 * pi));
+	double dE, current;
+	int i;
+	while (m_dtemperature > m_dtemperaturemin)
+	{
+		srand((unsigned int)time(0));
+		for (i = 0; i<num; i++)
+		{
+			//用当前时间点初始化随机种子，防止每次运行的结果都相同
+			
+			
+			current = func(rnd(0.0, 2 * pi));
+			dE = current - m_best;
+
+			if (dE < 0) //表达移动后得到更优解，则总是接受移动
+				m_best = current;
+			else
+			{
+				// 函数exp( dE/T )的取值范围是(0,1) ，dE/T越大，则exp( dE/T )也越大
+				if (exp(-dE / (m_dtemperature*k)) > rnd(0.0, 1.0))
+					m_best = current;
+			}
+		}
+		m_dtemperature *= m_drate;//降温退火 ，0<r<1 。r越大，降温越慢；r越小，降温越快
+		printf("最小值是 %f\n", m_best);
+	}
+	printf("最小值是 %f\n", m_best);
+	
+}
---
+++ AnnealingAlgorithm.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <random>
+#ifdef EXPORT
+#define EXORIM _declspec(dllexport)
+#else
+#define EXORIM _declspec(dllimport)
+#endif
+#define pi 3.141592653
+
+
+//返回指定范围内的随机浮点数
+double rnd(double dbLow, double dbUpper)
+{
+	double dbTemp = (double)rand() / ((double)RAND_MAX);
+	return dbLow + dbTemp*(dbUpper - dbLow);
+}
+double func(double x)//目标函数
+{
+	return 7 * sin(6 * x) + 6 * cos(5 * x);
+}
+
+
+class EXORIM CAnnealingAlgorithm
+{
+public:
+	CAnnealingAlgorithm();
+	virtual ~CAnnealingAlgorithm();
+
+public:
+	void Generate();
+
+protected:
+
+	int num = 80000; //迭代次数
+	double k = 0.1;    //常数
+	double m_drate = 0.9; //用于控制降温的快慢
+	double m_dtemperature = 20000; //系统的温度，系统初始应该要处于一个高温的状态
+	double m_dtemperaturemin = 0.2;//温度的下限，若温度T达到T_min，则停止搜索
+	double m_best;
+};
